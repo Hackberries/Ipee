9e09f99668632c4c6c2001a44d6b00947079d56f
Michele Beckmann <m.beckmann@live.de>
2021-02-03 14:05:47 +0100

    #11 Doku zu IPAddressStore hinzugefügt

--- Ipee/IPAddressStore.cs
+++ Ipee/IPAddressStore.cs
@@ -7,12 +7,24 @@ using System.Threading.Tasks;
 
 namespace Ipee
 {
+    /// <summary>
+    /// Regelt die Vergabe von IPv4-Adressen, hält diese, schlägt Adressen für neue Clients vor und stellt die Kompatibilität sicher.
+    /// </summary>
     public class IPAddressStore
     {
+        /// <summary>
+        /// Singleton Instanz der Klasse.
+        /// </summary>
         public static IPAddressStore Instance { get; } = new IPAddressStore();
 
         private List<IPAddress> addresses = new List<IPAddress>();
 
+        /// <summary>
+        /// Fügt eine neue IpAdresse hinzu.
+        /// </summary>
+        /// <exception cref="AdressAlreadyExistException"></exception>
+        /// <exception cref="NullReferenceException"></exception>
+        /// <param name="address">Die Adresse, welche hinzugefügt werden soll.</param>
         public void AddAddress(IPAddress address)
         {
             if (address is null)
