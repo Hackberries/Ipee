1eb13e9d17e2c85589b13aba9fc6710f69c75d1e
Michele Beckmann <m.beckmann@live.de>
2021-02-03 14:04:58 +0100

    #7 IPAddressStore hinzugefügt

---
+++ Ipee/IPAddressStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipee
+{
+    public class IPAddressStore
+    {
+        public static IPAddressStore Instance { get; } = new IPAddressStore();
+
+        private List<IPAddress> addresses = new List<IPAddress>();
+
+        public void AddAddress(IPAddress address)
+        {
+            if (address is null)
+                throw new NullReferenceException();
+
+            if (addresses.Contains(address))
+                throw new AdressAlreadyExistException();
+
+            this.addresses.Add(address);
+        }
+    }
+}
