617abf5a2aa99d46fc8112c36ce5f368dde64b27
Michele Beckmann <m.beckmann@live.de>
2021-02-04 13:35:10 +0100

    Create ConfigManagerTest.cs

---
+++ Ipee.Test/ConfigManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using Ipee.Core.DataPersistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ipee.Test
+{
+    public class ConfigManagerTest
+    {
+        [Fact]
+        public void CanBeCreated()
+        {
+            ConfigManager manager = new();
+        }
+    }
+}
