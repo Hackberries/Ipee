5d4b6969c03643c8bc6d8a1a4f84849f85c85fca
Michele Beckmann <m.beckmann@live.de>
2021-02-04 13:07:10 +0100

    Create AppStore.cs

---
+++ Ipee/Store/AppStore.cs
@@ -0,0 +1,23 @@
+﻿using Ipee.data_persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipee.Store
+{
+    /// <summary>
+    /// Der AppStore hält den gesammten Zustand der Anwendung und dient als Schnittstelle zwischen Logik und Oberfläche.
+    /// </summary>
+    public class AppStore
+    {
+        /// <summary>
+        /// Singleton Instanz der Klasse.
+        /// </summary>
+        public static AppStore Instance { get; set; } = new AppStore();
+
+        private IPAddressStore addressStore = new IPAddressStore();
+        private ConfigManager configManager = new ConfigManager();
+    }
+}
