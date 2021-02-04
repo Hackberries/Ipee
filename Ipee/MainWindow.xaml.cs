e718f754ebad5142e8ff0ee9a95807ba29f9508c
Flobodan <rebuke@outlook.de>
2021-02-04 13:11:27 +0100

    Textbox Validation

--- Ipee/MainWindow.xaml.cs
+++ Ipee/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Xps;
 
 namespace Ipee
 {
@@ -31,7 +33,32 @@ namespace Ipee
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            IPv6Label.Content = AdressConverter.IPtoHex(IPBox.Text);
+            IPAddress myAddress;           
+            var countDots = IPBox.Text.Split('.').Length - 1;
+            if (countDots == 3)
+            {
+                if (System.Net.IPAddress.TryParse(IPBox.Text, out myAddress))
+                {
+                    IPv6Label.Content = AdressConverter.IPtoHex(IPBox.Text);
+                }
+                else
+                {
+                    TextBlock txtBlock = new TextBlock();
+                    txtBlock.TextWrapping = TextWrapping.Wrap;
+                    txtBlock.Text = "Bitte geben Sie eine GÜLTIGE IPv4 Adresse ein!";
+                    IPv6Label.Content = txtBlock;
+                }
+            }
+            else
+            {
+                TextBlock txtBlock = new TextBlock();
+                txtBlock.TextWrapping = TextWrapping.Wrap;
+                txtBlock.Text = "Bitte geben Sie eine GÜLTIGE IPv4 Adresse ein!";
+                IPv6Label.Content = txtBlock;
+            }
+
+
+
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
