c4cde57dd5ef88f7c83f1402675cc01bb65514ef
Michele Beckmann <m.beckmann@live.de>
2021-02-04 13:35:07 +0100

    Create AdressConverterTest.cs

---
+++ Ipee.Test/AdressConverterTest.cs
@@ -0,0 +1,19 @@
+﻿using Ipee.Core.Converter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ipee.Test
+{
+    public class AdressConverterTest
+    {
+        [Fact]
+        public void CanConvertToHex()
+        {
+            Assert.Equal("0:0:0:0:0:FFFF:C0A8:01", AdressConverter.IPtoHex("192.168.0.1"));
+        }
+    }
+}
