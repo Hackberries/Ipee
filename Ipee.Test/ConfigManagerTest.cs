e1d2e7713f57d22c015ed0c7bce9eb93b82429fd
Michele Beckmann <m.beckmann@live.de>
2021-02-08 22:46:36 +0100

    Mehr Tests für ConfigManager

--- Ipee.Test/ConfigManagerTest.cs
+++ Ipee.Test/ConfigManagerTest.cs
@@ -39,5 +39,56 @@ namespace Ipee.Test
             ConfigManager manager = new(filepath);
             Assert.NotNull(manager.GetAllSubnetsAsList());
         }
+
+        [Fact]
+        public void CanAddItems()
+        {
+            ConfigManager manager = new(filepath);
+
+            var before = manager.GetAllSubnetsAsList().Count();
+            manager.AddSubnet("99.0.0.1", "Lorem Ipsum");
+            var after = manager.GetAllSubnetsAsList().Count();
+
+            Assert.True(after > before);
+        }
+
+        [Fact]
+        public void CanRemoveItems()
+        {
+            ConfigManager manager = new(filepath);
+
+            manager.AddSubnet("99.0.0.1", "Lorem Ipsum");
+            var before = manager.GetAllSubnetsAsList().Count();
+            manager.DeleteSubnet("99.0.0.1");
+            var after = manager.GetAllSubnetsAsList().Count();
+
+            Assert.True(after < before);
+        }
+
+        [Fact]
+        public void CanFindSubnetConfigBySubnetIp()
+        {
+            ConfigManager manager = new(filepath);
+
+            manager.AddSubnet("99.0.0.1", "Lorem Ipsum");
+            var found = manager.FindSubnetConfigBySubnetIp("99.0.0.1");
+
+            Assert.Equal("99.0.0.1", found.SubnetIp);
+        }
+
+        [Fact]
+        public void CanEditItems()
+        {
+            ConfigManager manager = new(filepath);
+
+            manager.AddSubnet("99.0.0.1", "Lorem Ipsum");
+            var configItem = manager.FindSubnetConfigBySubnetIp("99.0.0.1");
+            configItem.IpAddresses = new List<string>() { "1.1.1.1" };
+
+            manager.EditIpAddress("1.1.1.1", "2.2.2.2");
+            var found = manager.FindSubnetConfigByClientIp("2.2.2.2");
+
+            Assert.NotNull(found);
+        }
     }
 }
