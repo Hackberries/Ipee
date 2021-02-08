7d82f9a16e90670c56cec7967464ac1dbfa5af51
Michele Beckmann <m.beckmann@live.de>
2021-02-09 00:04:51 +0100

    #7 IPv4Adress Klasse erstellt + Tests

---
+++ Ipee.Core/Addressing/IPv4Address.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipee.Core.Addressing
+{
+    public class IPv4Address
+    {
+        private readonly byte[] octets;
+
+        public IPv4Address(string address)
+        {
+            this.octets = Parse(address);
+        }
+
+        public static byte[] Parse(string address)
+        {
+            var stringOctets = address.Split('.');
+
+            if (stringOctets.Length != 4)
+                throw new Exception();
+
+            var output = new List<byte>();
+
+            foreach (var octet in stringOctets)
+                output.Add(byte.Parse(octet));
+
+            return output.ToArray();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is IPv4Address address)
+            {
+                return this.octets[0] == address.octets[0]
+                    && this.octets[1] == address.octets[1]
+                    && this.octets[2] == address.octets[2]
+                    && this.octets[3] == address.octets[3];
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode() => HashCode.Combine(octets);
+
+        public override string ToString()
+        {
+            return $"{this.octets[0]}.{this.octets[1]}.{this.octets[2]}.{this.octets[3]}";
+        }
+    }
+}
---
+++ Ipee.Test/IPv4AddressTest.cs
@@ -0,0 +1,53 @@
+﻿using Ipee.Core.Addressing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ipee.Test
+{
+    public class IPv4AddressTest
+    {
+        [Theory]
+        [InlineData("192.168.0.1")]
+        [InlineData("192.168.0.255")]
+        [InlineData("253.21.161.14")]
+        [InlineData("78.208.127.94")]
+        [InlineData("217.76.148.213")]
+        public void IsConstructedCorrectly(string address)
+        {
+            var ip = new IPv4Address(address);
+            Assert.Equal(address, ip.ToString());
+        }
+
+        [Fact]
+        public void IsComparable()
+        {
+            var ip = new IPv4Address("253.21.161.14");
+            var ip2 = new IPv4Address("253.21.161.14");
+            var ip3 = new IPv4Address("192.168.0.1");
+            Assert.Equal(ip, ip2);
+            Assert.NotEqual(ip, ip3);
+        }
+
+        [Theory]
+        [InlineData("253.21.161.14", new byte[] { 253, 21, 161, 14 })]
+        [InlineData("192.168.0.1", new byte[] { 192, 168, 0, 1 })]
+        [InlineData("78.208.127.94", new byte[] { 78, 208, 127, 94 })]
+        public void CanParseFromString(string input, byte[] excpectedBytes)
+        {
+            var octets = IPv4Address.Parse(input);
+
+            for (int index = 0; index < octets.Length; index++)
+                Assert.Equal(excpectedBytes[index], octets[index]);
+        }
+
+        [Fact]
+        public void ThrowsParseException()
+        {
+            Assert.ThrowsAny<Exception>(() => new IPv4Address("abc.lorem.ipsum.WerDasLießtIstDoof"));
+        }
+    }
+}
