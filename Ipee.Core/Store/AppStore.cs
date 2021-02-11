defafbb6e68155e4be65532e713beb5dcbe66345
Michele Beckmann <m.beckmann@live.de>
2021-02-11 01:17:17 +0100

    IPAddressStore gelöscht

--- Ipee.Core/Store/AppStore.cs
+++ Ipee.Core/Store/AppStore.cs
@@ -1,4 +1,6 @@
-﻿using Ipee.Core.DataPersistence;
+﻿using Ipee.Core.Addressing;
+using Ipee.Core.DataPersistence;
+using Ipee.Core.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,7 @@ namespace Ipee.Core.Store
 {
     /// <summary>
     /// Der AppStore hält den gesammten Zustand der Anwendung und dient als Schnittstelle zwischen Logik und Oberfläche.
+    /// Regelt ebenso die Vergabe von IPv4-Adressen, hält diese, schlägt Adressen für neue Clients vor und stellt die Kompatibilität sicher.
     /// </summary>
     public class AppStore
     {
@@ -17,12 +20,40 @@ namespace Ipee.Core.Store
         /// </summary>
         public static AppStore Instance { get; set; } = new AppStore();
 
-        private IPAddressStore addressStore = new IPAddressStore();
+        private List<IPv4Address> addresses = new();
+
+        private List<IPv4Subnet> subnets = new();
+
         public ConfigManager ConfigManager;
 
         public void Initialized(string configPath)
         {
             this.ConfigManager = new ConfigManager(configPath);
+        }
+
+        /// <summary>
+        /// Fügt eine neue IpAdresse hinzu.
+        /// </summary>
+        /// <exception cref="AddressAlreadyExistException"></exception>
+        /// <exception cref="NullReferenceException"></exception>
+        /// <param name="address">Die Adresse, welche hinzugefügt werden soll.</param>
+        public void AddAddress(IPv4Address address)
+        {
+            if (address is null)
+                throw new NullReferenceException();
+
+            if (addresses.Contains(address))
+                throw new AddressAlreadyExistException();
+
+            this.addresses.Add(address);
+        }
+
+        public void AddSubnet(IPv4Subnet subnet)
+        {
+            if (subnet is null)
+                throw new NullReferenceException();
+
+            this.subnets.Add(subnet);
         }
     }
 }
